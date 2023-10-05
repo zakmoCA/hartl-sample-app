f10a693d81b2605a9175767bdcb10ede1f04c0d0
zakmoCA <zakeriyaamohamed@gmail.com>
2023-10-05 19:57:25 +1000

    initial fialing test to fetch about page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,4 +10,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_help_url
     assert_response :success
   end
+
+  test "should get about" do
+    get static_pages_help_about
+    assert_response :success
+  end
 end
